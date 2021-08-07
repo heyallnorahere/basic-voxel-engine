18fee9a261ffa261aa41ff6cc9e7f572ae17fd4b
yodasoda1219 <theo.github@bedafamily.com>
2021-08-06 23:13:42 -0700

    initial commit

---
+++ src/include/bve_pch.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <cstdint>
+#include <stddef.h>
---
+++ src/main.cpp
@@ -0,0 +1,4 @@
+#include "bve_pch.h"
+int main(int argc, const char** argv) {
+    return 0;
+}
