c9243aa64fb88dfde917bd7afb8b0b91d8284aab
yodasoda1219 <theo.github@bedafamily.com>
2021-08-18 01:35:20 -0700

    finish managed wrapper classes

--- src/include/code_host.h
+++ src/include/code_host.h
@@ -1,6 +1,5 @@
 #pragma once
 namespace bve {
-    class code_host;
     namespace managed {
         class wrapper : public ref_counted {
         public:
@@ -14,17 +13,31 @@ namespace bve {
         };
         class object : public wrapper {
         public:
-            static void check_exception(ref<object> exception);
+            static void handle_exception(ref<object> exception);
             object(MonoObject* _object, MonoDomain* domain);
+            object(const std::string& string, MonoDomain* domain);
             virtual ~object() override;
+            ref<object> get(MonoClassField* field);
+            ref<object> get(MonoProperty* property);
+            void set(MonoClassField* field, void* value);
+            void set(MonoProperty* property, void* value);
+            std::string get_string();
+            template<typename T> T unbox() {
+                MonoObject* _object = mono_gchandle_get_target(this->m_handle);
+                void* pointer = mono_object_unbox(_object);
+                return *(T*)pointer;
+            }
             template<typename... Args> ref<object> invoke(MonoMethod* method, Args*... args) {
+                if (!method) {
+                    throw std::runtime_error("[managed object] attempted to call nullptr!");
+                }
                 MonoObject* _object = mono_gchandle_get_target(this->m_handle);
                 std::vector<void*> args_vector = { std::forward<Args*>(args)... };
                 MonoObject* exc = nullptr;
                 MonoObject* returned = mono_runtime_invoke(method, _object, args_vector.data(), &exc);
                 if (exc) {
                     ref<object> exception = ref<object>::create(exc, this->get_domain());
-                    check_exception(exception);
+                    handle_exception(exception);
                 }
                 ref<object> returned_object;
                 if (returned) {
@@ -40,6 +53,28 @@ namespace bve {
         class class_ : public wrapper {
         public:
             class_(MonoClass* _class, MonoDomain* domain);
+            std::string get_name();
+            void get_name(std::string& namespace_name, std::string& class_name);
+            MonoMethod* get_method(const std::string& descriptor);
+            MonoClassField* get_field(const std::string& name);
+            MonoProperty* get_property(const std::string& name);
+            template<typename... Args> ref<object> invoke(MonoMethod* method, Args*... args) {
+                if (!method) {
+                    throw std::runtime_error("[managed class] attempted to call nullptr!");
+                }
+                std::vector<void*> args_vector = { std::forward<Args*>(args)... };
+                MonoObject* exc = nullptr;
+                MonoObject* returned = mono_runtime_invoke(method, nullptr, args_vector.data(), &exc);
+                if (exc) {
+                    ref<object> exception = ref<object>::create(exc, this->get_domain());
+                    object::handle_exception(exception);
+                }
+                ref<object> returned_object;
+                if (returned) {
+                    returned_object = ref<object>::create(returned, this->get_domain());
+                }
+                return returned_object;
+            }
             virtual void* get() override;
             virtual MonoImage* get_image() override;
         private:
@@ -58,14 +93,16 @@ namespace bve {
         };
         class assembly : public wrapper {
         public:
+            static ref<assembly> get_assembly(ref<wrapper> wrapper_object);
             assembly(MonoAssembly* _assembly, MonoDomain* domain);
             ref<class_> get_class(const std::string& name);
+            ref<class_> get_class(const std::string& namespace_name, const std::string& class_name);
+            MonoMethod* get_method(const std::string& descriptor);
             virtual void* get() override;
-            virtual MonoImage* get_image() = 0;
+            virtual MonoImage* get_image() override;
         private:
             MonoAssembly* m_assembly;
             MonoImage* m_image;
-            friend class ::bve::code_host;
         };
     }
 }
--- src/lib/code_host.cpp
+++ src/lib/code_host.cpp
@@ -18,15 +18,60 @@ namespace bve {
         MonoDomain* wrapper::get_domain() {
             return this->m_domain;
         }
-        void object::check_exception(ref<object> exception) {
+        void object::handle_exception(ref<object> exception) {
             ref<class_> exception_class = ref<class_>::create(mono_get_exception_class(), exception->get_domain());
             // todo: read data
         }
         object::object(MonoObject* _object, MonoDomain* domain) : wrapper(domain) {
             this->m_handle = mono_gchandle_new(_object, false);
         }
+        object::object(const std::string& string, MonoDomain* domain) : wrapper(domain) {
+            MonoString* str = mono_string_new(domain, string.c_str());
+            this->m_handle = mono_gchandle_new((MonoObject*)str, false);
+        }
         object::~object() {
             mono_gchandle_free(this->m_handle);
+        }
+        ref<object> object::get(MonoClassField* field) {
+            MonoObject* _object = mono_gchandle_get_target(this->m_handle);
+            MonoObject* value = mono_field_get_value_object(this->get_domain(), field, _object);
+            ref<object> returned_object;
+            if (value) {
+                returned_object = ref<object>::create(value, this->get_domain());
+            }
+            return returned_object;
+        }
+        ref<object> object::get(MonoProperty* property) {
+            MonoObject* _object = mono_gchandle_get_target(this->m_handle);
+            MonoObject* exc = nullptr;
+            MonoObject* value = mono_property_get_value(property, _object, nullptr, &exc);
+            if (exc) {
+                ref<object> exception = ref<object>::create(exc, this->get_domain());
+                handle_exception(exception);
+            }
+            ref<object> returned_object;
+            if (value) {
+                returned_object = ref<object>::create(value, this->get_domain());
+            }
+            return returned_object;
+        }
+        void object::set(MonoClassField* field, void* value) {
+            MonoObject* _object = mono_gchandle_get_target(this->m_handle);
+            mono_field_set_value(_object, field, value);
+        }
+        void object::set(MonoProperty* property, void* value) {
+            MonoObject* _object = mono_gchandle_get_target(this->m_handle);
+            MonoObject* exc = nullptr;
+            mono_property_set_value(property, _object, nullptr, &exc);
+            if (exc) {
+                ref<object> exception = ref<object>::create(exc, this->get_domain());
+                handle_exception(exception);
+            }
+        }
+        std::string object::get_string() {
+            MonoObject* _object = mono_gchandle_get_target(this->m_handle);
+            MonoString* string = (MonoString*)_object;
+            return std::string(mono_string_to_utf8(string));
         }
         void* object::get() {
             return mono_gchandle_get_target(this->m_handle);
@@ -38,6 +83,25 @@ namespace bve {
         }
         class_::class_(MonoClass* _class, MonoDomain* domain) : wrapper(domain) {
             this->m_class = _class;
+        }
+        std::string class_::get_name() {
+            std::string namespace_name, class_name;
+            this->get_name(namespace_name, class_name);
+            return namespace_name + "." + class_name;
+        }
+        void class_::get_name(std::string& namespace_name, std::string& class_name) {
+            namespace_name = mono_class_get_namespace(this->m_class);
+            class_name = mono_class_get_name(this->m_class);
+        }
+        MonoMethod* class_::get_method(const std::string& descriptor) {
+            MonoMethodDesc* desc = mono_method_desc_new(descriptor.c_str(), false);
+            return mono_method_desc_search_in_class(desc, this->m_class);
+        }
+        MonoClassField* class_::get_field(const std::string& name) {
+            return mono_class_get_field_from_name(this->m_class, name.c_str());
+        }
+        MonoProperty* class_::get_property(const std::string& name) {
+            return mono_class_get_property_from_name(this->m_class, name.c_str());
         }
         void* class_::get() {
             return this->m_class;
@@ -59,6 +123,12 @@ namespace bve {
             MonoClass* _class = mono_type_get_class(this->m_type);
             return mono_class_get_image(_class);
         }
+        ref<assembly> assembly::get_assembly(ref<wrapper> wrapper_object) {
+            MonoImage* image = wrapper_object->get_image();
+            MonoAssembly* _assembly = mono_image_get_assembly(image);
+            MonoDomain* domain = wrapper_object->get_domain();
+            return ref<assembly>::create(_assembly, domain);
+        }
         assembly::assembly(MonoAssembly* _assembly, MonoDomain* domain) : wrapper(domain) {
             this->m_assembly = _assembly;
             this->m_image = mono_assembly_get_image(this->m_assembly);
@@ -66,8 +136,15 @@ namespace bve {
         ref<class_> assembly::get_class(const std::string& name) {
             std::string namespace_name, class_name;
             parse_name(name, namespace_name, class_name);
+            return this->get_class(namespace_name, class_name);
+        }
+        ref<class_> assembly::get_class(const std::string& namespace_name, const std::string& class_name) {
             MonoClass* _class = mono_class_from_name(this->m_image, namespace_name.c_str(), class_name.c_str());
             return ref<class_>::create(_class, this->get_domain());
+        }
+        MonoMethod* assembly::get_method(const std::string& descriptor) {
+            MonoMethodDesc* desc = mono_method_desc_new(descriptor.c_str(), false);
+            return mono_method_desc_search_in_image(desc, this->m_image);
         }
         void* assembly::get() {
             return this->m_assembly;
