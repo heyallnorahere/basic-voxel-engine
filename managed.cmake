43f41771b6745f361d594dfb7f392ecb46df38c1
yodasoda1219 <theo.github@bedafamily.com>
2021-08-25 23:10:24 -0700

    fix cmake copying

--- managed.cmake
+++ managed.cmake
@@ -1,5 +1,5 @@
 add_custom_target(managed-code ALL
     COMMAND dotnet build -o ${CMAKE_CURRENT_BINARY_DIR}/managed-code
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_BINARY_DIR}/managed-code/*.dll ${CMAKE_CURRENT_SOURCE_DIR}/src
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_BINARY_DIR}/managed-code ${CMAKE_CURRENT_SOURCE_DIR}/src
     DEPENDS basic-voxel-engine
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
