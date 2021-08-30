0c8c1a7d02931225c14d355f2e595acd9889ece3
yodasoda1219 <theo.github@bedafamily.com>
2021-08-30 13:05:03 -0700

    add csproj to solution on windows and add folders

--- managed.cmake
+++ managed.cmake
@@ -1,4 +1,10 @@
-add_custom_target(managed-code ALL
-    COMMAND dotnet build -o ${CMAKE_CURRENT_BINARY_DIR}/managed-code
-    DEPENDS basic-voxel-engine
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+if(WIN32)
+    include_external_msproject(BasicVoxelEngine ${CMAKE_CURRENT_SOURCE_DIR}/BasicVoxelEngine/BasicVoxelEngine.csproj)
+    set(MANAGED_TARGET_NAMES BasicVoxelEngine)
+else()
+    add_custom_target(managed-code ALL
+        COMMAND dotnet build -o ${CMAKE_CURRENT_BINARY_DIR}/managed-code
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    set(MANAGED_TARGET_NAMES managed-code)
+endif()
+set_target_properties(${MANAGED_TARGET_NAMES} PROPERTIES FOLDER BVE)
