575fda180841c582b27c9b2b8af27ad410555a16
yodasoda1219 <theo.github@bedafamily.com>
2021-09-02 19:59:34 -0700

    update msbuild command

--- managed.cmake
+++ managed.cmake
@@ -3,8 +3,14 @@ if(WIN32)
     include_external_msproject(BasicVoxelEngine.Content ${CMAKE_CURRENT_SOURCE_DIR}/BasicVoxelEngine.Content/BasicVoxelEngine.Content.csproj)
     set(MANAGED_TARGET_NAMES BasicVoxelEngine BasicVoxelEngine.Content)
 else()
+    find_file(MSBUILD_ASSEMBLY MSBuild.dll PATHS "/usr/lib/mono/msbuild/Current/bin" "/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/msbuild/Current/bin")
+    if(MSBUILD_ASSEMBLY)
+        message(STATUS "Found MSBuild: ${MSBUILD_ASSEMBLY}")
+    else()
+        message(FATAL_ERROR "Could not find MSBuild. Is mono installed?")
+    endif()
     add_custom_target(managed-code ALL
-        COMMAND msbuild -v:q
+        COMMAND mono ${MSBUILD_ASSEMBLY} -v:q
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
     set(MANAGED_TARGET_NAMES managed-code)
 endif()
